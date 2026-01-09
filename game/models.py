2a8aa627601ae35a42333ddc8ea02e633ed751f4
Jakub Zaręba <jzareba2@st.swps.edu.pl>
2026-01-09 09:55:15 +0100

    Migracje + Model Hero

--- game/models.py
+++ game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+"The Player Character. Relation: One-to-One with User. "
+
+class Hero(models.Model):    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    hp = models.IntegerField(default=100)
+    max_hp = models.IntegerField(default=100)
+    xp = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    x_pos = models.IntegerField(default=0)
+    y_pos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} (Lvl {self.level})"
+    
